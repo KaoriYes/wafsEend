f1b17ce65590d8bd24789a0c0679068d76b058ae
KaoriYes <Quinten.kok@hva.nl>
2024-02-14 23:46:21 +0100

    Oops

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -16,7 +16,7 @@ async function fetchDataColabs() {
         const response = await fetch('https://api.github.com/repos/Kaoriyes/wafsEend/collaborators', {
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28',
-                'Authorization': 'Bearer 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',
+                'Authorization': 'Bearer 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ds',
                 'Accept': 'application/vnd.github+json',
             }
         })
